c4cd91ff0241b3a19100c9f2365165c5f04a7044
Timm Friebe <thekid@thekid.de>
2013-08-04 12:43:51 +0200

    - Add more tests for nextLine / hasMoreLines

--- src/test/php/net/daringfireball/markdown/unittest/InputTest.class.php
+++ src/test/php/net/daringfireball/markdown/unittest/InputTest.class.php
@@ -16,13 +16,47 @@ abstract class InputTest extends \unittest\TestCase {
   protected abstract function newFixture($source);
 
   #[@test]
-  public function empty_input() {
-    $this->assertEquals(null, $this->newFixture('')->nextLine());
+  public function has_empty_input() {
+    $this->assertFalse($this->newFixture('')->hasMoreLines());
+  }
+
+  #[@test]
+  public function has_empty_input_twice() {
+    $fixture= $this->newFixture('');
+    $this->assertFalse($fixture->hasMoreLines(), '#1');
+    $this->assertFalse($fixture->hasMoreLines(), '#2');
+  }
+
+  #[@test]
+  public function read_empty_input() {
+    $this->assertNull($this->newFixture('')->nextLine());
+  }
+
+  #[@test]
+  public function read_empty_input_twice() {
+    $fixture= $this->newFixture('');
+    $this->assertNull($fixture->nextLine(), '#1');
+    $this->assertNull($fixture->nextLine(), '#2');
   }
 
   #[@test, @values(array("Test\n", "Test\r\n", "Test\r"))]
-  public function one_line($value) {
+  public function has_one_line($value) {
+    $this->assertTrue($this->newFixture($value)->hasMoreLines());
+  }
+
+  #[@test, @values(array("Test\n", "Test\r\n", "Test\r"))]
+  public function read_one_line($value) {
     $this->assertEquals(new Line('Test'), $this->newFixture($value)->nextLine());
+  }
+
+  #[@test]
+  public function has_one_line_without_line_ending() {
+    $this->assertTrue($this->newFixture('Test')->hasMoreLines());
+  }
+
+  #[@test]
+  public function read_one_line_without_line_ending() {
+    $this->assertEquals(new Line('Test'), $this->newFixture('Test')->nextLine());
   }
 
   #[@test, @values(array("Line 1\nLine 2\n", "Line 1\r\nLine 2\r\n", "Line 1\rLine 2\r"))]
@@ -32,5 +66,27 @@ abstract class InputTest extends \unittest\TestCase {
       array(new Line('Line 1'), new Line('Line 2')),
       array($fixture->nextLine(), $fixture->nextLine())
     );
+  }
+
+  #[@test]
+  public function has_no_more_lines_after_reading_one_line() {
+    $fixture= $this->newFixture("Test\n");
+    $fixture->nextLine();
+    $this->assertFalse($fixture->hasMoreLines());
+  }
+
+  #[@test]
+  public function has_more_lines_after_reading_first_of_two_lines() {
+    $fixture= $this->newFixture("Line 1\nLine 2\n");
+    $fixture->nextLine();
+    $this->assertTrue($fixture->hasMoreLines());
+  }
+
+  #[@test]
+  public function has_no_more_lines_after_reading_two_lines() {
+    $fixture= $this->newFixture("Line 1\nLine 2\n");
+    $fixture->nextLine();
+    $fixture->nextLine();
+    $this->assertFalse($fixture->hasMoreLines());
   }
 }
