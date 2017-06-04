c348da0e68bc1d9baa22ccf7a6e2fdd07d2998e4
Timm Friebe <thekid@thekid.de>
2017-06-04 19:53:19 +0200

    QA: Add apidocs

--- src/main/php/net/daringfireball/markdown/Ruler.class.php
+++ src/main/php/net/daringfireball/markdown/Ruler.class.php
@@ -1,20 +1,28 @@
 <?php namespace net\daringfireball\markdown;
 
+/**
+ * A (horizontal) ruler
+ *
+ * @test  xp://net.daringfireball.markdown.unittest.RulerTest
+ */
 class Ruler extends Node {
 
-  public function emit($definitions) {
+  /**
+   * Emit this node
+   *
+   * @param  net.daringfireball.markdown.Emitter $emitter
+   * @param  [:net.daringfireball.markdown.Link] $definitions
+   * @return string
+   */
+  public function emit($emitter, $definitions= []) {
     return '<hr/>';
   }
 
   /** @return string */
-  public function toString() {
-    return nameof($this);
-  }
+  public function toString() { return nameof($this); }
 
   /** @return string */
-  public function hashCode() {
-    return '---';
-  }
+  public function hashCode() { return '---'; }
 
   /**
    * Returns whether a given comparison value is equal to this node list
@@ -22,7 +30,5 @@ class Ruler extends Node {
    * @param  var $value
    * @return string
    */
-  public function compareTo($value) {
-    return $value instanceof self ? 0 : 1;
-  }
+  public function compareTo($value) { return $value instanceof self ? 0 : 1; }
 }
