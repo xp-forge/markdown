27d1fdd5cc14d2ab471b6ff6ff198e1066c42891
Timm Friebe <thekid@thekid.de>
2019-03-28 10:59:06 +0100

    Refactor: Extract base class URL

--- src/main/php/net/daringfireball/markdown/Image.class.php
+++ src/main/php/net/daringfireball/markdown/Image.class.php
@@ -1,22 +1,6 @@
 <?php namespace net\daringfireball\markdown;
 
-use util\Objects;
-
-class Image extends Node {
-  public $url, $text, $title;
-
-  /**
-   * Creates a new image element
-   *
-   * @param  string $url
-   * @param  net.daringfireball.markdown.Node $text Optional text
-   * @param  string $title Optional title
-   */
-  public function __construct($url, Node $text= null, $title= null) {
-    $this->url= $url;
-    $this->text= $text;
-    $this->title= $title;
-  }
+class Image extends URL {
 
   /**
    * Emit this node
@@ -27,37 +11,5 @@ class Image extends Node {
    */
   public function emit($emitter, $definitions= []) {
     return $emitter->emitImage($this, $definitions);
-  }
-
-  /** @return string */
-  public function toString() {
-    return sprintf(
-      '%s(url= %s, text= %s, title= %s)',
-      nameof($this),
-      $this->url,
-      null === $this->text ? 'null' : $this->text->toString(),
-      null === $this->title ? 'null' : '"'.$this->title.'"'
-    );
-  }
-
-  /** @return string */
-  public function hashCode() {
-    return '!'.Objects::hashOf([$this->url, $this->text, $this->title]);
-  }
-
-  /**
-   * Returns whether a given comparison value is equal to this node list
-   *
-   * @param  var $value
-   * @return string
-   */
-  public function compareTo($value) {
-    return $value instanceof self
-      ? Objects::compare(
-        [$this->url, $this->title, $this->text],
-        [$value->url, $value->title, $value->text]
-      )
-      : 1
-    ;
   }
 }
---
+++ src/main/php/net/daringfireball/markdown/URL.class.php
@@ -0,0 +1,57 @@
+<?php namespace net\daringfireball\markdown;
+
+use util\Objects;
+
+abstract class URL extends Node {
+  public $url, $text, $title;
+
+  /**
+   * Creates a new image element
+   *
+   * @param  string $url
+   * @param  net.daringfireball.markdown.Node $text Optional text
+   * @param  string $title Optional title
+   */
+  public function __construct($url, Node $text= null, $title= null) {
+    $this->url= $url;
+    $this->text= $text;
+    $this->title= $title;
+  }
+
+  /** @return ?string */
+  public function reference() {
+    return '@' === $this->url{0} ? substr($this->url, 1) : null;
+  }
+
+  /** @return string */
+  public function toString() {
+    return sprintf(
+      '%s(url= %s, text= %s, title= %s)',
+      nameof($this),
+      $this->url,
+      null === $this->text ? 'null' : $this->text->toString(),
+      null === $this->title ? 'null' : '"'.$this->title.'"'
+    );
+  }
+
+  /** @return string */
+  public function hashCode() {
+    return static::class[0].Objects::hashOf([$this->url, $this->text, $this->title]);
+  }
+
+  /**
+   * Returns whether a given comparison value is equal to this node list
+   *
+   * @param  var $value
+   * @return string
+   */
+  public function compareTo($value) {
+    return $value instanceof self
+      ? Objects::compare(
+        [$this->url, $this->title, $this->text],
+        [$value->url, $value->title, $value->text]
+      )
+      : 1
+    ;
+  }
+}
